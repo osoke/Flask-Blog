774389a75826e3057a44ddca8456ba94db47a087
admin <me@email.com>
2022-06-08 23:21:41 +0800

    creat_all() init DB

--- main.py
+++ main.py
@@ -204,5 +204,6 @@ def delete_post(post_id):
 
 if __name__ == "__main__":
     db.create_all() # 初始化DB
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
+    # app.run(host='0.0.0.0', port=5000)
     # app.run(debug=True)
